da36bffe1cc4ded95b39d412c84a26ee55f76934
Zack Fernandes <zack@ohnoco.com>
2013-07-11 16:55:37 -0700

    Cleaned up Application layout, added the title helper.

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,10 @@
 module ApplicationHelper
+  # Public: Sets the yield(:title) block, for use in application.html.erb
+  #   page_title - String for the page title
+  #
+  # Returns an HTML-safe string.
+  def title(page_title)
+    content_for(:title) { h(page_title.to_s) }
+    content_tag :h1, page_title
+  end
 end
