c941214814a2f7fbe764c4dd52514a1313cb10c0
Zack Fernandes <zack@ohnoco.com>
2013-07-12 02:24:34 -0700

    Created some little user session helpers.

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -7,4 +7,18 @@ module ApplicationHelper
     content_for(:title) { h(page_title.to_s) }
     content_tag :h1, page_title
   end
+
+  # Public: Returns the current user, if someone's logged in.
+  #
+  # Returns a User
+  def current_user
+    @current_user ||= User.find_by(id: session[:user_id]) if session[:user_id]
+  end
+
+  # Public: Check if there's currently a user session.
+  #
+  # Returns boolean.
+  def user_signed_in?
+    session[:user_id].present?
+  end
 end
