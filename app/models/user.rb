7e81cd383ad8483703eb76e64e651c4a8245dfa5
Zack Fernandes <zack@ohnoco.com>
2013-07-12 02:28:13 -0700

    Started writing the methods we'll use with Legato.
    Because we're using OmniAuth for our initial Google signin, we don't
    have OAuth2 objects generated to pass to Legato. These methods use the
    existing user token to generate a new OAuth2 AccessToken object that
    Legato can use for our requests.

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,7 @@
 class User < ActiveRecord::Base
+  # Public: Finds or creates a User by the UID returned from Google OAuth
+  #
+  # Returns a User
   def self.login_from_google(auth)
     user = find_by(uid: auth['uid'])
     if user.blank?
@@ -6,10 +9,27 @@ class User < ActiveRecord::Base
         user.uid = auth['uid']
         user.email = auth['info']['email']
         user.token = auth['credentials']['token']
-        user.token_expires_at = auth['credentials']['expires_at']
+        user.token_expires_at = Time.strptime(auth['credentials']['expires_at'].to_s, '%s')
       end
     else
       user
     end
+  end
+
+  # Public: Generates an OAuth2 Token Object.
+  # We'll use this within the Legato Gem to access Google Analytics.
+  #
+  def oauth2_token
+    @client = OAuth2::Client.new(BUZZ_CONFIG[:GOOGLE_KEY], BUZZ_CONFIG[:GOOGLE_SECRET], {
+      authorize_url: 'https://accounts.google.com/o/oauth2/auth',
+      token_url: 'https://accounts.google.com/o/oauth2/token'
+    })
+    OAuth2::AccessToken.from_hash(@client, { access_token: token })
+  end
+
+  # Public: Generates a Legato::User using this user's Google token.
+  #
+  def build_legato_user
+    Legato::User.new(oauth2_token)
   end
 end
